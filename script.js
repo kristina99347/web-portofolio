0f36df923d6009a57e19b7ae1c62533a3039999c
Kris Tina <kristina123@gmail.com>
2025-12-24 13:39:53 +0700

    first commit portfolio

---
+++ script.js
@@ -0,0 +1,7 @@
+// window.addEventListener("load", () => {
+//     document.querySelectorAll(".fade").forEach(el => {
+//         el.style.transition = "0.8s";
+//         el.style.opacity = 1;
+//         el.style.transform = "translateY(0)";
+//     });
+// });
